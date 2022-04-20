7e6686000155bbc85b73189a41ab1b7b8f1b0e79
Alexis Guzman <alexis12989x@gmail.com>
2022-04-20 13:50:09 -0500

    feat: config explorers put path

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ app.post('/v1/explorers', (req, res) => {
   res.status(201).json({ message: 'Created', data: requestBody });
 });
 
+app.put('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers POST request ${new Date()}`);
+  console.log(`Update explorer with id ${req.params.id}`);
+  const requestBody = req.body;
+
+  res.status(200).json({ message: 'Updated', date: requestBody });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

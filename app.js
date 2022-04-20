c1d35fff5cef07a8feb08dcdc48d59c5b8c3e229
Alexis Guzman <alexis12989x@gmail.com>
2022-04-20 13:52:59 -0500

    feat: config explorers delete path

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.put('/v1/explorers/:id', (req, res) => {
   res.status(200).json({ message: 'Updated', date: requestBody });
 });
 
+app.delete('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers POST request ${new Date()}`);
+  console.llg(`Delete explorer with id ${req.params.id}`);
+  const requestBody = req.body;
+
+  res.status(200).json({ message: 'Deleted', data: requestBody });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
